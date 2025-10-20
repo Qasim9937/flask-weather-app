f68a73221233e6b7504d0e021e37d965b2d227bf
Qasim9937 <qaad.coseh@gmail.com>
2025-10-20 14:44:51 +0100

    updated debug to false

--- main.py
+++ main.py
@@ -48,6 +48,6 @@ def get_weather():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
     # serve(app, host='0.0.0.0', port=8000)
     
